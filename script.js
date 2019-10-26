a50ed34559e39e200f06d0ee4abc07326e368179
Becca Burke <reburke286@gmail.com>
2019-10-26 12:22:00 -0700

    finalized script for saving localStorage

--- script.js
+++ script.js
@@ -3,49 +3,48 @@ $(document).ready(function(){
 
 
     function addDomainToModal(domain) {
-        // variable to store string from textarea
-        var name = $("#textarea2").val();
-    
         // variable to create new li element
         var li = $("<li>");
-        li.html(name + "   <button class=\"delete-button\">Delete</button>");
-        li.id = domainNames.length;
-        // pushing textarea input to array
-        domainNames.push(name);
+        li.html(domain + "   <button class=\"delete-button\">Delete</button>");
+        // li.id = domain;
         // appending li to div
         $("#ul-modal").append(li);
+    };
+
+    function getDomainsFromLocalStorage() {
+        var domains = JSON.parse(localStorage.getItem("domain"));
+        if (!domains) {
+            domains = [];
+        }
+        return domains;
     }
 
-    var domainNames = ["becca-burke.com"];
-
-    function addDomainNamesFromStorage(event) {
-        event.preventDefault();
-        localStorage.getItem("domain");
-
-        addDomainToModal(domainNames); 
+    function addDomainNamesToModal() {
+      
+        var domains = getDomainsFromLocalStorage();
+        
+        for (var i = 0; i < domains.length; i++) {
+            addDomainToModal(domains[i]); 
+        }
     }
 
-    addDomainNamesFromStorage();
+    addDomainNamesToModal();
 
-    function addDomainName(event) {
-        //grab the domain name that was just added
-        // add it to the array of all domainNames (hint: you can do this from local storage - no global vars necessary)
-        // add it to the modal
+    function addNewDomainToLocalStorage(domain) {
+        //getting domains from local storage
+        var domains = getDomainsFromLocalStorage();
+        domains.push(domain);
         // save it to local storage
-
+        localStorage.setItem("domain", JSON.stringify(domains));
     }
 
-    
-          // function addPersonToList(event) {
-//     event.preventDefault(); 
-//     var name = nameEl.value;
-//     var li = document.createElement("li");
-//     li.id = people.length;
-//     li.innerHTML = name + " <button>edit</button>";
-//     people.push({ name: name });
-//     peopleListEl.append(li);
-//   }  
-
+    function addDomainName() {
+        //grab the domain name that was just added
+        var name = $("#textarea2").val();
+        // add it to the modal
+        addDomainToModal(name);
+        addNewDomainToLocalStorage(name);
+    }
 
     // add button - adds domain name to modal & array
     $('#add-btn').on("click", addDomainName);
